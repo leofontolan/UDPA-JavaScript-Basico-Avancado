ce6ebc9e466284db89546d1c43d182a88fcac75a
leofontolan <leofontolan@gmail.com>
2022-06-04 17:12:15 -0300

    Aula 10

---
+++ aula10/index.js
@@ -0,0 +1,22 @@
+const nome = 'Leo'; // String
+const nome1 = "Leonardo!"; // String
+const nome2 = `Leonardo!`; // String
+
+const num1 = 10; // Number
+const num2 = 20.65; // Number com ponto flutuante
+
+let aluno; // Undefined -> não aponto para local nenhum na memória
+let sobrenomeAluno = null; // Null -> aponta para local nenhum na memória
+
+
+console.log(typeof nome, nome);
+console.log(typeof nome1, nome1);
+console.log(typeof nome2, nome2);
+console.log('----------------------------\n');
+
+console.log(typeof num1, num1);
+console.log(typeof num2, num2);
+console.log('----------------------------\n');
+
+console.log(typeof aluno, aluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
