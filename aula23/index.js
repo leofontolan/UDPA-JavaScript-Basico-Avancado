b3bbecea729a81f18d57b9c01b52f953f89e96aa
leofontolan <leofontolan@gmail.com>
2022-06-07 11:07:01 -0300

    Execicio Aula 23

---
+++ aula23/index.js
@@ -0,0 +1,26 @@
+//Operadores de comparação
+// > maior que
+console.log(1 > 2); // false
+
+// < menor que
+console.log(1 < 2); // true
+
+// >= maior ou igual
+console.log(1 >= 2); // false
+
+// <= menor ou igual
+console.log(1 <= 2); // true
+
+// == igual
+const v1 = 1;
+const v2 = '1';
+console.log(v1 == v2); // true
+
+// === igual e do mesmo tipo
+console.log(v1 === v2); // false
+
+// != diferente
+console.log(v1 != v2); // false
+
+// !== diferente e do mesmo tipo
+console.log(v1 !== v2); // true
