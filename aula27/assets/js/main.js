17cf9247481e415c0cae236124ad5c01b06d15f2
leofontolan <leofontolan@gmail.com>
2022-06-08 10:02:57 -0300

    Exercicio

--- aula27/assets/js/main.js
+++ aula27/assets/js/main.js
@@ -17,34 +17,34 @@ function calculadoraIMC() {
 
 
         resultado.style.display = 'block';
-        
-        if (resultadoIMC < 18.5) {
-            resultado.innerHTML = `<p>Você está abaixo do peso</p>`;
+
+        if (resultadoIMC >= 10 && resultadoIMC < 18.5) {
+            resultado.innerHTML = `<p>Seu ICM é ${resultadoIMC}, você está abaixo do peso</p>`;
             resultado.style.backgroundColor  = 'orange';
             resultado.style.color = 'red';
 
         }else if (resultadoIMC >= 18.5 && resultadoIMC < 25) {
-            resultado.innerHTML = `<p>Você está com o peso ideal</p>`;
+            resultado.innerHTML = `<p>Seu ICM é ${resultadoIMC}, você está com o peso ideal</p>`;
             resultado.style.backgroundColor  = 'green';
             resultado.style.color = 'white';
 
         }else if (resultadoIMC >= 25 && resultadoIMC < 30) {
-            resultado.innerHTML = `<p>Você está com sobrepeso</p>`;
+            resultado.innerHTML = `<p>Seu ICM é ${resultadoIMC}, você está com sobrepeso</p>`;
             resultado.style.backgroundColor  = 'orange';
             resultado.style.color = 'red';
 
         }else if(resultadoIMC >= 30 && resultadoIMC < 35) {
-            resultado.innerHTML = `<p>Você está com obesidade grau 1</p>`;
+            resultado.innerHTML = `<p>Seu ICM é ${resultadoIMC}, você está com obesidade grau 1</p>`;
             resultado.style.backgroundColor  = 'red';
             resultado.style.color = 'white';
 
         }else if (resultadoIMC >= 35 && resultadoIMC < 40) {
-            resultado.innerHTML = `<p>Você está com obesidade grau 2</p>`;
+            resultado.innerHTML = `<p>Seu ICM é ${resultadoIMC}, você está com obesidade grau 2</p>`;
             resultado.style.backgroundColor  = 'red';
             resultado.style.color = 'white';
 
-        }else if (resultadoIMC >= 40) {
-            resultado.innerHTML = `<p>Você está com obesidade grau 3</p>`;
+        }else if (resultadoIMC >= 40 && resultadoIMC < 45) {
+            resultado.innerHTML = `<p>Seu ICM é ${resultadoIMC}, você está com obesidade grau 3</p>`;
             resultado.style.backgroundColor  = 'red';
             resultado.style.color = 'white';
 
