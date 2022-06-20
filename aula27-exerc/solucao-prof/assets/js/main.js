1f8c64c37f15479f97fee95e7eae48690b8ee7d9
leofontolan <leofontolan@gmail.com>
2022-06-20 11:06:01 -0300

    Resoltao Prof

--- aula27-exerc/solucao-prof/assets/js/main.js
+++ aula27-exerc/solucao-prof/assets/js/main.js
@@ -1,46 +1,76 @@
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const inputPeso = event.target.querySelector('#peso');
-    const inputAltura = event.target.querySelector('#altura');
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
+  const inputPeso = event.target.querySelector("#peso");
+  const inputAltura = event.target.querySelector("#altura");
 
-    if(!peso){
-        setResult('Peso Inválido', false);
-        return;
-    }
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
-    if(!altura){
-        setResult('Altura Inválida', false);
-        return;
-    }
+  if (!peso) {
+    setResult("Peso Inválido", false);
+    return;
+  }
 
-    const imc = getImc(peso, altura);
-    console.log(imc);
+  if (!altura) {
+    setResult("Altura Inválida", false);
+    return;
+  }
+
+  const imc = getImc(peso, altura);
+  const indexImc = getIndexlImc(imc);
+  const msg = `Seu IMC é ${imc} (${indexImc})`;
+
+  setResult(msg, true);
 });
 
-
-function criaP(){
-    const p = document.createElement('p');
-    return p;
+function criaP() {
+  const p = document.createElement("p");
+  return p;
 }
 
+function setResult(msg, isValid) {
+  const result = document.querySelector("#result");
+  result.innerHTML = "";
 
-function setResult(msg, isValid){
-    const result = document.querySelector('#result');
-    result.innerHTML = '';
-    
-    const p = criaP();
-    p.innerHTML = msg;
-    result.appendChild(p);
+  const p = criaP();
 
+  if (isValid) {
+    p.classList.add("paragrafo-resultado");
+  } else {
+    p.classList.add("bad");
+  }
+
+  p.innerHTML = msg;
+  result.appendChild(p);
 }
 
-function getImc(peso, altura){
-    const imc = peso / (altura * altura); 
-    return imc.toFixed(2);
+function getImc(peso, altura) {
+  const imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
+
+function getIndexlImc(imc) {
+  const index = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc > 39.9) return index[5];
+
+  if (imc >= 34.9) return index[4];
+
+  if (imc >= 29.9) return index[3];
+
+  if (imc >= 24.9) return index[2];
+
+  if (imc >= 18.5) return index[1];
+
+  if (imc < 18.5) return index[0];
 }
