25a65a6c5b6daa07cda8a2098764878d7e0a22aa
leofontolan <leofontolan@gmail.com>
2022-06-20 13:27:26 -0300

    Date String

--- aula29/date.js
+++ aula29/date.js
@@ -6,3 +6,7 @@ console.log(dataZero.toString());
 
 const tresHoras = new Date(3 * 3600 * 1000);
 console.log(tresHoras.toString());
+
+
+const dataAbril = new Date(2019, 3, 20, 15,30,00);
+console.log(dataAbril.toString());
