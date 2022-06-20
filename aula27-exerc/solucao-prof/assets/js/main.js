fb551ee5445e62a734e4f9289d984e4c341a6d43
leofontolan <leofontolan@gmail.com>
2022-06-20 10:23:53 -0300

    Imc

--- aula27-exerc/solucao-prof/assets/js/main.js
+++ aula27-exerc/solucao-prof/assets/js/main.js
@@ -1,53 +1,46 @@
 const form = document.querySelector('#form');
-const finalResult = document.querySelector('#result');
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function(event) {
     event.preventDefault();
+    
+    const inputPeso = event.target.querySelector('#peso');
+    const inputAltura = event.target.querySelector('#altura');
 
-    result = parseFloat(calcImc(form.peso.value, form.altura.value)).toFixed(2);
-    showResult(result);
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if(!peso){
+        setResult('Peso Inválido', false);
+        return;
+    }
+
+    if(!altura){
+        setResult('Altura Inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    console.log(imc);
 });
 
-function calcImc(peso, altura){
-    const imc = peso / (altura * altura);
-    return imc;
-};
+
+function criaP(){
+    const p = document.createElement('p');
+    return p;
+}
 
 
-function showResult(result){
-
-
-    //Limpar Classes
-    if(finalResult.classList.value !== ''){
-        finalResult.classList.remove(finalResult.classList.value);
-    }
-
-    if(result >= 15  && result < 18.5){
-        finalResult.innerHTML = `Seu IMC é ${result}, você está abaixo do peso`;
-        finalResult.classList.add('low-imc');
-        
-
-    }else if(result >= 18.5 && result < 25){
-        finalResult.innerHTML = `Seu IMC é ${result}, você está no peso normal`; 
-        finalResult.classList.add('medium-imc');
-
-    }else if(result >= 25 && result < 29.9){
-        finalResult.innerHTML = `Seu IMC é ${result}, você está acima do peso`;
-        finalResult.classList.add('high-imc');
-
-    }else if(result >= 29.9 && result < 34.9){
-        finalResult.innerHTML = `Seu IMC é ${result}, você está com obesidade grau I`;
-        finalResult.classList.add('high-imc');
+function setResult(msg, isValid){
+    const result = document.querySelector('#result');
+    result.innerHTML = '';
     
-    }else if(result >= 34.9 && result < 39.9){
-        finalResult.innerHTML = `Seu IMC é ${result}, você está com obesidade grau II`;
-        finalResult.classList.add('high-imc');
+    const p = criaP();
+    p.innerHTML = msg;
+    result.appendChild(p);
 
-    } else if (result >= 40 && result < 49.9){
-        finalResult.innerHTML = `Seu IMC é ${result}, você está com obesidade grau III`;
-        finalResult.classList.add('high-imc');
-    
-    }else{
-        finalResult.innerHTML = `Você digitou um valor inválido`;
-    }
+}
+
+function getImc(peso, altura){
+    const imc = peso / (altura * altura); 
+    return imc.toFixed(2);
 }
