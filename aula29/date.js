66ac53d479488e81a25ba6145ced3ad85b533611
leofontolan <leofontolan@gmail.com>
2022-06-20 11:40:05 -0300

    Objeto Date

---
+++ aula29/date.js
@@ -0,0 +1,8 @@
+const data = new Date();
+console.log(data.toString());
+
+const dataZero = new Date(0);
+console.log(dataZero.toString());
+
+const tresHoras = new Date(3 * 3600 * 1000);
+console.log(tresHoras.toString());
