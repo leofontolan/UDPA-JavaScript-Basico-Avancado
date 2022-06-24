a2c57d21cb0e728f4b7e5439f6945b79a60301e8
leofontolan <leofontolan@gmail.com>
2022-06-24 17:08:21 -0300

    Destructuring

---
+++ aula32/var-let.js
@@ -0,0 +1,22 @@
+// const verdadeira = true;
+
+// var nome1 = 'nome 1 - A';
+// var nome1 = 'nome 1 - B';
+
+
+// if(verdadeira) {
+//     var nome1 = 'nome 1 - C';
+//     console.log(nome1);
+
+//     if(verdadeira) {
+//         var nome1 = 'nome 1 - D';
+//         console.log(nome1);
+//     }
+// }
+
+function falaNome(){
+    var nome = 'Leo Fontolan'
+    console.log(nome)
+}
+
+falaNome();
