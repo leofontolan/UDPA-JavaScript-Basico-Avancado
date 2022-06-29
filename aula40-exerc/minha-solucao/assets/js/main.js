fffffd2660febf20dc9ed002e7047ab74101ce84
leofontolan <leofontolan@gmail.com>
2022-06-29 17:18:08 -0300

    Minha Solucao

---
+++ aula40-exerc/minha-solucao/assets/js/main.js
@@ -0,0 +1,9 @@
+const estilosBody = getComputedStyle(document.body);
+const bgColor = estilosBody.backgroundColor;
+
+const ps =  document.querySelectorAll('.paragrafos p');
+
+
+for (let p of ps){
+    p.style.backgroundColor = bgColor;
+}
