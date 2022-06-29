8b4308cf9b46e85b1501fbd8f86949997f9c29e2
leofontolan <leofontolan@gmail.com>
2022-06-29 15:26:30 -0300

    For Of

---
+++ aula39/for-of.js
@@ -0,0 +1,17 @@
+const first_name = 'Leo Fontolan';
+
+/* // Iterando com For clássico
+for (let i = 0; i < first_name.length ; i++) {
+    console.log(first_name[i]);
+}
+
+
+//Iterando com For In
+for (let key in first_name) {
+    console.log(first_name[key]);
+}
+ */
+//Iterando com  For Of
+for (let valor of first_name){
+    console.log(valor);
+}
