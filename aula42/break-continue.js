0a89b1cb0caa5d0488f872379613b30e15e3832b
leofontolan <leofontolan@gmail.com>
2022-06-30 10:51:31 -0300

    Break Continue

---
+++ aula42/break-continue.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+for (number of numbers) {
+
+    if(number === 2){
+        continue;
+    }
+
+    if(number === 5){
+        break;
+    }
+
+    console.log(number);
+}
+
