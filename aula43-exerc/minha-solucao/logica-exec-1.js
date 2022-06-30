fa4c2638f70e16c9de246b6e1e1e45d97ea6f888
leofontolan <leofontolan@gmail.com>
2022-06-30 11:12:41 -0300

    Resolucao Exercicios

---
+++ aula43-exerc/minha-solucao/logica-exec-1.js
@@ -0,0 +1,7 @@
+// Escreva una função que recebe 2 números e retorne o maior deles.
+
+function findMaxNumber(num1, num2){
+     return Math.max(num1, num2);
+}
+
+console.log(findMaxNumber(6000, 456));
---
+++ aula43-exerc/solucao-prof-1/logica-prog-1.js
@@ -0,0 +1,9 @@
+function max(x, y){
+    return x > y ? x : y;
+}
+
+// console.log(max(10, 200000));
+
+
+const max2 = (x,y) => x > y ? x : y;
+console.log(max2(10, 200000));
