36d2064b7fc6b1f4e23dcd12684ee7de1662a7d9
leofontolan <leofontolan@gmail.com>
2022-07-04 15:24:14 -0300

    Exer

--- aula50-exerc/resolucao-prof/assets/js/main-list.js
+++ aula50-exerc/resolucao-prof/assets/js/main-list.js
@@ -2,10 +2,22 @@ const inputTarefa = document.querySelector('.input-nova-tarefa');
 const btnAdicionar = document.querySelector('.btn-add-tarefa');
 const listaTarefas = document.querySelector('.tarefas');
 
+function criarLi(){
+    const li = document.createElement('li');
+    return li;
+}
+
 
 function criaTarefa(textoInput){
-    console.log(textoInput);
+    const li = criarLi();
+    li.innerText = textoInput;
+    listaTarefas.appendChild(li);
+    limpaInput();
+}
 
+function limpaInput(){
+    inputTarefa.value = '';
+    inputTarefa.focus();
 }
 
 
@@ -14,6 +26,13 @@ btnAdicionar.addEventListener('click', function(event) {
     if(!inputTarefa.value) return;
         criaTarefa(inputTarefa.value);
 
+});
+
+inputTarefa.addEventListener('keypress', function(event) {
+    if(event.keyCode === 13){
+        if(!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
 });
 
 
