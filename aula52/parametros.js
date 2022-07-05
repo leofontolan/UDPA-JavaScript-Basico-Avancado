80a1b109a543d5791c90735245f406a4edd655b0
leofontolan <leofontolan@gmail.com>
2022-07-05 11:23:27 -0300

    Argumentos com valores padrao

--- aula52/parametros.js
+++ aula52/parametros.js
@@ -1,12 +1,24 @@
-function sum(a, b){
-    return a + b;
+function multiply(a, b){
+    return a * b;
 }
 sum(2,2);
 
 
 
 function teste(){
-    console.log("teste");
+    console.log(arguments);
 }
-
 teste('valor', 1,2,3,4,5,6,7,8,9,10);
+
+
+
+function menosArgumentos(a, b, c, d, e, f, g, h, i) {
+    console.log(a, b, c, d, e, f, g, h, i);
+}
+menosArgumentos(1,2,3);
+
+
+function sum(a, b = 0){
+    return a + b;
+}
+console.log(sum(2));
