082bbbd88927786e2541b489b8c953099ccdf765
leofontolan <leofontolan@gmail.com>
2022-07-06 12:23:24 -0300

    IIFE

---
+++ aula57/iife.js
@@ -0,0 +1,14 @@
+(function(){
+
+
+    const name = 'Leo';
+    const surname = 'Fontolan';
+
+    function showFullName(){
+        console.log(`${name} ${surname}`);
+    }
+
+
+    showFullName();
+
+})();
