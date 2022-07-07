6c4ffea1c434f560a066a9a985abd41047121afe
leofontolan <leofontolan@gmail.com>
2022-07-07 10:08:27 -0300

    Factory

--- aula58/factory.js
+++ aula58/factory.js
@@ -1,4 +1,4 @@
-function createPerson(name, surname){
+function createPerson(name, surname, hgt, wgt){
 
     return{
         name: 'Maria',
@@ -6,9 +6,21 @@ function createPerson(name, surname){
 
         showName(msn){
             return `${msn}, ${this.name} ${this.surname}`
+        },
+
+        height: hgt,
+        weight: wgt,
+
+        imc(){
+            const index = this.weight / (this.height ** 2)
+            return index.toFixed(2);
         }
+
     }
 }
 
-const p1 = createPerson('Leo', 'Fontolan');
+const p1 = createPerson('Leo', 'Fontolan', 1.72, 90);
 console.log(p1.showName('Olá'));
+console.log(p1.imc());
+
+const p2 = createPerson('Maria', 'Almeida', 1.60, 46);
