de01fdd2e31440b4a164a4ef4db492ad2da06420
leofontolan <leofontolan@gmail.com>
2022-07-07 13:21:18 -0300

    Botao Delete

--- aula59-exerc/solucao-prof-1/assets/js/main.js
+++ aula59-exerc/solucao-prof-1/assets/js/main.js
@@ -6,28 +6,38 @@ function criarCalculadora(){
 
 
         inicia(){
-            alert('Oi Calculadora Iniciada');
+            this.cliqueBotoes();
         },
+
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+
 
         cliqueBotoes(){
 
-            document.addEventListener('click', function(e){
-            
+            document.addEventListener('click', (e) => {    
                 const el = e.target;
-                
-                if(el.classList.contains('btn-num')){
-                    this.btnParaDisplay(el.innerText);    
-                };
-
-            }.bind(this));
+            
+                if(el.classList.contains('btn-num')) this.btnParaDisplay(el.innerText);    
+                if(el.classList.contains('btn-clear')) this.clearDisplay();
+                if(el.classList.contains('btn-del')) this.deleteLast();
+        
+            });
         },
 
         btnParaDisplay(valor){
             this.display.value += valor;
+        },
+
+
+        deleteLast(){
+            this.display.value = this.display.value.slice(0, - 1);
         }
+
     };
 }
 
 const calculadora = criarCalculadora();
-calculadora.cliqueBotoes();
-
+calculadora.inicia();
