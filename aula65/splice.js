b3af15c3e25af7788963d7c3fc1091e364a72e5a
leofontolan <leofontolan@gmail.com>
2022-07-12 11:46:24 -0300

    splice

---
+++ aula65/splice.js
@@ -0,0 +1,8 @@
+//               -5       -4       -3         -2        -1
+//                0        1        2          3         4          
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+
+
+nomes.splice(3, 0, 'Leonardo', 'Juliana', 'Gustavo');
+console.log(nomes);
