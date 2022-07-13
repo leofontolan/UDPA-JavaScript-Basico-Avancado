677f2d1a0527d69b5b373a8d772eee177b2c5cd7
leofontolan <leofontolan@gmail.com>
2022-07-13 14:16:06 -0300

    forEach

--- aula71/forEach.js
+++ aula71/forEach.js
@@ -1,20 +1,20 @@
 const a1 = [1,2, 3, 4, 5, 6, 7, 8, 9];
 
 // Iterar tendo com resultado o valor
-// for (let valor of a1) {
-//     console.log(valor);
-// }
+for (let valor of a1) {
+    console.log(valor);
+}
 
 // Iterar tendo com resultado o índice
-// for (let valor in a1){
-//     console.log(`${valor} ${a1[valor]}`);
-// }   
+for (let valor in a1){
+    console.log(`${valor} ${a1[valor]}`);
+}   
 
 
 //forEach
-// a1.forEach(function(valor, indice, array){ 
-//     console.log(`${valor} ${indice} ${array}`); 
-// });
+a1.forEach(function(valor, indice, array){ 
+    console.log(`${valor} ${indice} ${array}`); 
+});
 
 
 const a2 = [10 ,20 , 30, 40, 50, 60, 70, 80, 90];
