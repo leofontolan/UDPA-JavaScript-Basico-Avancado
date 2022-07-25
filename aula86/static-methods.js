762d56b7e271078a22ca2063af7f5bc233be7982
leofontolan <leofontolan@gmail.com>
2022-07-25 14:57:25 -0300

    Static methods

--- aula86/static-methods.js
+++ aula86/static-methods.js
@@ -12,4 +12,20 @@ class RemoteControl{
     turnDownVolume(){
         this.volume -= 2;
     }
+
+    static changeFrequency(frequency){
+        console.log(`Changing frequency to ${frequency}`);
+    }
 }
+
+const c1 = new RemoteControl('LG');
+const c2 = new RemoteControl('Samsung');
+c1.turnUpVolume();
+c2.turnUpVolume();
+
+console.log(c1);
+console.log(c2);
+
+
+RemoteControl.changeFrequency(120);
+c2.changeFrequency(600);
