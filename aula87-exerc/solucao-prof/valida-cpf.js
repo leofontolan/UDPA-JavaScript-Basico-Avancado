ae548c51b1bb691d7399edba484a2c0877a29c5e
leofontolan <leofontolan@gmail.com>
2022-07-26 11:21:03 -0300

    Validador de CPF com Classes

--- aula87-exerc/solucao-prof/valida-cpf.js
+++ aula87-exerc/solucao-prof/valida-cpf.js
@@ -10,6 +10,27 @@ class ValidaCPF{
     }
 
 
+    geraNovoCpf(){
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1);
+        this.novoCpf = cpfSemDigitos + digito1 + digito2;
+    }
+
+    geraDigito(cpfSemDigitos){
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let stringNumerica of cpfSemDigitos){
+            total += reverso * Number(stringNumerica)
+            reverso--;
+        }
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
+    }
+
+
     validandoSequencia(){
         return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
     }
@@ -19,18 +40,21 @@ class ValidaCPF{
         if(typeof this.cpfLimpo !== 'string') return false;
         if(this.cpfLimpo.length !== 11) return false;
         if(this.validandoSequencia()) return false;
+        this.geraNovoCpf();
 
-        return 'CHEGUEI AQUI';
+        return this.novoCpf === this.cpfLimpo;
     }
 }
 
-let validaCPF = new ValidaCPF('070.987.720-03');
-console.log(validaCPF.valida());
-
-validaCPF = new ValidaCPF('999.999.999-99');
-console.log(validaCPF.valida());
+let validaCPF = new ValidaCPF('705.484.450-52');
 
 
+if(validaCPF.valida()){
+    console.log('CPF válido');
+}else{
+    console.log('CPF inválido');
+
+}
 
 //CPFs para testar:
 //705.484.450-52
