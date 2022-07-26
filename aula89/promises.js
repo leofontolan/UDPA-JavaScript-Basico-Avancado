3e1980b9b5972adf6436ce647df88590b1c15bcc
leofontolan <leofontolan@gmail.com>
2022-07-26 20:04:24 -0300

    Promises

--- aula89/promises.js
+++ aula89/promises.js
@@ -1,16 +1,34 @@
-function aleatorio(min, max){
-    min *= 1000;
-    max *= 1000;
-    return Math.floor(Math.random() * (max - min) + min);
+function aleatorio(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-
-function aguardar(msg, tempo){
+function aguardar(msg, tempo) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log(msg)
+      resolve(msg);
     }, tempo);
+  });
 }
 
-aguardar('Frase 1', aleatorio(1, 5));
-aguardar('Frase 2', aleatorio(1, 5));
-aguardar('Frase 3', aleatorio(1, 5));
+aguardar("Frase 1", aleatorio(1, 5))
+  .then((answer) => {
+    console.log(answer);
+    return aguardar("Frase 2", aleatorio(1, 5));
+  })
+
+  .then((answer) => {
+    console.log(answer);
+    return aguardar("Frase 3", aleatorio(1, 5));
+  })
+
+  .then((answer) => {
+    console.log(answer);
+
+  })
+  .then(() => {
+    console.log("Fim");
+  });
+
+  console.log("Isso será exibido antes de todas as promises");
