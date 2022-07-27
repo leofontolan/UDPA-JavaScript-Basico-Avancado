b3c7570f6b7c92f9c59e70228284ac3a0551e366
leofontolan <leofontolan@gmail.com>
2022-07-27 11:34:12 -0300

    Promises

--- aula89/promises.js
+++ aula89/promises.js
@@ -6,6 +6,10 @@ function aleatorio(min, max) {
 
 function aguardar(msg, tempo) {
   return new Promise((resolve, reject) => {
+
+
+    if(typeof msg !== 'string') reject(new Error('ERRO: msg deve ser uma string'));
+
     setTimeout(() => {
       resolve(msg);
     }, tempo);
@@ -20,15 +24,18 @@ aguardar("Frase 1", aleatorio(1, 5))
 
   .then((answer) => {
     console.log(answer);
-    return aguardar("Frase 3", aleatorio(1, 5));
+    return aguardar(111, aleatorio(1, 5));
   })
 
   .then((answer) => {
     console.log(answer);
-
+    image.png
   })
   .then(() => {
     console.log("Fim");
+
+  }).catch((err) => {
+    console.log('ERROS', err);
   });
 
   console.log("Isso será exibido antes de todas as promises");
