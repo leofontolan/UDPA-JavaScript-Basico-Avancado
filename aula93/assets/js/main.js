4fc1ddce8a2848b1ab697641d4c0b0b47e51d8c5
leofontolan <leofontolan@gmail.com>
2022-07-28 12:51:27 -0300

    Fetch API

---
+++ aula93/assets/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === "a") {
+    e.preventDefault();
+    loadPage(el);
+  }
+});
+
+async function loadPage(el) {
+
+  try {
+
+    const href = el.getAttribute("href");
+    const response = await fetch(href);
+
+    if (response.status !== 200) throw new Error("Erro ao carregar página");
+
+    const html = await response.text();
+    loadResult(html);
+    
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function loadResult(response) {
+  const result = document.querySelector(".result");
+  result.innerHTML = response;
+}
