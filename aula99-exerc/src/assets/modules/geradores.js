8472fd3b287178772f13dc10eca0c3fb27c50e84
leofontolan <leofontolan@gmail.com>
2022-08-04 11:20:56 -0300

    Gerador de Senhas Exercicio

---
+++ aula99-exerc/src/assets/modules/geradores.js
@@ -0,0 +1,27 @@
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumero = () => String.fromCharCode(rand(48, 58));
+const simbolos = '!@#$%¨&*()_+-=[]{}|;:,.<>/?';
+const geraSimbolo = () => simbolos[rand(0, simbolos.length)];
+
+export default function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos){
+    const senhaArray = [];
+    qtd = Number(qtd);
+
+    for(let i=0; i < qtd; i++){
+        if(maiusculas){
+            senhaArray.push(geraMaiuscula());
+        }
+        if(minusculas){
+            senhaArray.push(geraMinuscula());
+        }
+        if(numeros){
+            senhaArray.push(geraNumero());
+        }
+        if(simbolos){
+            senhaArray.push(geraSimbolo());
+        }
+    }
+    return senhaArray.join('').slice(0, qtd);
+}
